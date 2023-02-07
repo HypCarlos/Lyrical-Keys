e4623f27eeac93a19ee596b7490e6538f39e1c50
unknown <ccord042@fiu.edu>
2023-02-06 23:55:20 -0500

    changes to improve page responsiveness and word generation

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import randomWords from "random-words";
 import "./homepage.css";
 
+const NUMB_OF_WORDS = 200;
+const SECONDS = 60;
+
 const Homepage = () => {
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    setWords(generateWords());
+  }, []);
+
+  function generateWords() {
+    return new Array(NUMB_OF_WORDS).fill(null).map(() => randomWords());
+  }
+
   return (
     <div className="homepage-container">
       <div className="search-container">
-        <div className="search-border">
-          <h3 id="search">SEARCH</h3>
-        </div>
+        <input type="text" placeholder="Search"></input>
       </div>
       <div className="vinyl-container">
         <h2>[VINYL]</h2>
       </div>
       <div className="lyrics-container">
-        <h3 id="lyrics">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.
-        </h3>
+        {words.map((word, i) => (
+          <>
+            <h3 id="lyrics">{word}</h3>
+            <span> </span>
+          </>
+        ))}
       </div>
     </div>
   );
